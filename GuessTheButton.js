9a1c1a83924671b51983cecd95e594443f4425c0
Vlad Burcea <93820932+burceavlad93@users.noreply.github.com>
2022-05-09 20:31:54 +0300

    Update GuessTheButton.js

--- GuessTheButton.js
+++ GuessTheButton.js
@@ -1,9 +1,19 @@
-const random = Math.floor(Math.random() * (3 - 1 + 1) ) + 1;
-function randomDraw(buttonNumber) {
-  if (random == buttonNumber) {
-  	alert("CONGRATULATIONS! Winner Winner Chicken Dinner");
-  } else {
-  	alert("It was button #" + random + ". Click OK to Try again!");
-  }
-  location.reload();
+let number = Math.trunc(Math.random() * 3) + 1;
+const btns = document.querySelectorAll('.btn');
+const resetGame = document.querySelector('.again');
+
+for (let i = 0; i < btns.length; ++i) {
+    btns[i].addEventListener('click', function () {
+        if (number === i) {
+            document.body.style.backgroundColor = '#60b347';
+        } else {
+            document.body.style.backgroundColor = '#222';
+        }
+    })
+
 }
+
+resetGame.addEventListener('click', function () {
+    document.body.style.backgroundColor = '#222';
+    number = Math.trunc(Math.random() * 3) + 1;
+});
